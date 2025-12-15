6a0f33a14bfc98974c8004c4123f9d8fa4c0665d
Kalle Kristensen <kalle.bulow@gmail.com>
2025-12-15 15:53:18 +0100

    Add myFood.R – fetch data from Salling Group Food Waste API

---
+++ myFood.R
@@ -0,0 +1,15 @@
+library(rvest)
+library(httr)
+library(dplyr)
+
+args=commandArgs()
+
+base=args[1]
+
+url="https://api.sallinggroup.com/v1/food-waste"
+resraw=GET(url=url)
+rescontent=content(resraw, as="text")
+
+
+rescook=resraw$cookies
+print("done")
